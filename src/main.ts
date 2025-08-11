d0112bb7025218b65b300f78ed5a1bcb13edaff8
munesyasin1 <munes.yasin@joacademy.com>
2025-08-11 23:01:39 +0300

    bugfix/JA2-564 : fix

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:8080'], // your frontend port here
+    origin: ['http://localhost:8080', 'https://timecraft-flow.vercel.app/'], // your frontend port here
     credentials: true, // allow cookies or Authorization headers
   });
 
